0c970c99d0a7a1f22ddb67184e04501b86e5176e
Federico Ramos <federico.ramos@wazuh.com>
2025-07-21 22:20:39 -0300

    Add basic main.py with a main window

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from pycroglia!")
+from PyQt6 import QtWidgets
 
+from pycroglia.ui.widgets.main_stack import MainStack
 
 if __name__ == "__main__":
-    main()
+    app = QtWidgets.QApplication([])
+    wizard = MainStack()
+    wizard.setWindowTitle("Main Window")
+    screen = app.primaryScreen().availableGeometry()
+    wizard.resize(int(screen.width() * 0.75), int(screen.height() * 0.75))
+    wizard.show()
+    app.exec()
