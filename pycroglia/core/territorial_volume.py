e123a71f85ce98a22189a3058bbab5ef6184a3c0
Juan <jaguirreb@fi.uba.ar>
2025-10-26 19:06:54 -0300

    Fix linter checks

--- pycroglia/core/territorial_volume.py
+++ pycroglia/core/territorial_volume.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from typing import Any
 import numpy as np
 from numpy.typing import NDArray
--- pycroglia/core/tests/unit/test_full_cell_analysis.py
+++ pycroglia/core/tests/unit/test_full_cell_analysis.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-from pycroglia.core.full_cell_analysis import FullCellAnalysis, AnalysisResult
-
+from pycroglia.core.full_cell_analysis import FullCellAnalysis
 
 def test_full_cell_analysis():
     """Test FullCellAnalysis computes convex volumes and complexities.
@@ -41,10 +40,6 @@ def test_full_cell_analysis():
     fca = FullCellAnalysis(masks, voxscale)
     result = fca.compute()
     expected_cell_volumes=np.array([0.4, 0.4])
-    expected_convex_simplices=[
-        np.array([[2, 3, 0], [1, 3, 0], [1, 2, 0], [1, 2, 3]], dtype=np.int32),
-        np.array([[2, 1, 0], [3, 1, 0], [3, 2, 0], [3, 2, 1]], dtype=np.int32),
-    ]
     expected_convex_vertices=[
         np.array([0, 1, 2, 3], dtype=np.int32),
         np.array([0, 1, 2, 3], dtype=np.int32),
