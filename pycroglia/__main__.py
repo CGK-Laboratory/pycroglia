ef337f9026db98d4394ac8e59f307e3143f5f26e
Juan Aguirre <65740935+jab227@users.noreply.github.com>
2025-12-05 16:10:13 -0300

    Fixed main so that it can be run with uvx

--- pycroglia/__main__.py
+++ pycroglia/__main__.py
@@ -3,7 +3,7 @@ from PyQt6 import QtWidgets
 from pycroglia.ui.widgets.wizard.config import DEFAULT_CONFIG
 from pycroglia.ui.widgets.wizard.wizard import ConfigurableMainStack
 
-if __name__ == "__main__":
+def main():
     app = QtWidgets.QApplication([])
     wizard = ConfigurableMainStack(config=DEFAULT_CONFIG)
     wizard.setWindowTitle("Pycroglia")
@@ -11,3 +11,6 @@ if __name__ == "__main__":
     wizard.resize(int(screen.width() * 0.75), int(screen.height() * 0.75))
     wizard.show()
     app.exec()
+
+if __name__ == "__main__":
+    main()
