14c18bc0871e314392224f7e0da67770fff67a8a
Fernando J. Chaure <5598671+ferchaure@users.noreply.github.com>
2026-05-16 13:14:15 -0300

    ascii labels

--- pycroglia/core/plot/skeleton.py
+++ pycroglia/core/plot/skeleton.py
@@ -96,7 +96,7 @@ class SkeletonCellPlot:
             plotter.camera_position = "xy"  # Equivalent to MATLAB view(0,270)
             plotter.add_axes()
             plotter.show_grid(
-                xtitle="X (µm)", ytitle="Y (µm)", ztitle="Z (µm)", color="gray"
+                xtitle="X (um)", ytitle="Y (um)", ztitle="Z (um)", color="gray"
             )
             plotter.set_background("white")
             plotter.enable_eye_dome_lighting()
